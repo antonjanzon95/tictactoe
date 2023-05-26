09f63e3b1a9d8eb6fd2a7bfc5df141c75abb4f23
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-05-26 13:38:18 +0200

    added grid component

---
+++ src/components/Grid.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Square from './Square.vue';
+import { ref } from 'vue';
+
+const squares = ref<number>(9);
+</script>
+
+<template>
+  <div class="w-60 h-60 flex flex-wrap inset-0 m-auto">
+    <Square v-for="square in squares" shape="X" />
+  </div>
+</template>
