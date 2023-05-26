d8c319c56ec17523c0170582af563327c30609a6
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-05-26 14:13:23 +0200

    added square comp, props, emits, interface

--- src/components/Square.vue
+++ src/components/Square.vue
@@ -3,13 +3,23 @@ import { ref } from 'vue';
 
 interface IShape {
   shape: 'X' | 'O';
+  index: number;
 }
 
 const props = defineProps<IShape>();
+
+const emits = defineEmits(['placeShape']);
+
+const handleClick = () => {
+  emits('placeShape', props.index);
+};
 </script>
 
 <template>
-  <div class="w-20 h-20 outline">
-    {{ !shape ? '' : shape }}
+  <div
+    class="w-20 h-20 text-6xl outline flex justify-center items-center"
+    @click="handleClick"
+  >
+    {{ shape }}
   </div>
 </template>
