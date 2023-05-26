0e2d943fdef3019a7e1e33f419ce7a590cb32dc1
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-05-26 13:38:27 +0200

    added square component

---
+++ src/components/Square.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+interface IShape {
+  shape: 'X' | 'O';
+}
+
+const props = defineProps<IShape>();
+</script>
+
+<template>
+  <div class="w-20 h-20 outline">
+    {{ !shape ? '' : shape }}
+  </div>
+</template>
