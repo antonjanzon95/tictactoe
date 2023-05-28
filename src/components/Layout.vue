ab2628ede5f9acb035b311b93f1b2b775512fcb7
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-05-28 17:56:57 +0200

    added layout component

---
+++ src/components/Layout.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Header from './Header.vue';
+</script>
+
+<template>
+  <Header />
+
+  <main class="h-screen flex justify-center items-center">
+    <slot></slot>
+  </main>
+</template>
+
+<style scoped></style>
