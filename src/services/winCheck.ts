1624518208ca3a92267cdc87a5d43222756af527
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-05-28 19:48:58 +0200

    added wincheck func

---
+++ src/services/winCheck.ts
@@ -0,0 +1,21 @@
+// import { Game } from '../models/CGame';
+// import { CSquare } from '../models/CSquare';
+
+// export const winningCombinations = [
+//   [0, 1, 2],
+//   [3, 4, 5],
+//   [6, 7, 8],
+//   [0, 3, 6],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [0, 4, 8],
+//   [2, 4, 6],
+// ];
+
+// export const winCheck = (game: Game) => {
+//   winningCombinations.forEach((combination) => {
+//     combination.forEach((square, index) => {
+//       if
+//     })
+//   })
+// }
