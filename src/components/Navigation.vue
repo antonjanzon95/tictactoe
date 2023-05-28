dc27dde84af66450cf093fb926df892231522f12
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-05-28 17:56:42 +0200

    added navigation component

---
+++ src/components/Navigation.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const links = [
+  { path: '/', name: 'Home' },
+  { path: '/about', name: 'About' },
+  { path: '/game', name: 'Game' },
+];
+</script>
+
+<template>
+  <nav class="flex justify-center items-center h-full bg-slate-100">
+    <ul class="flex gap-8">
+      <li v-for="link in links">
+        <router-link :to="link.path">{{ link.name }}</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped></style>
