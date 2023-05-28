76a675582533bb1632e95ccd6c4a7837715bd06e
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-05-28 18:47:33 +0200

    added game class

---
+++ src/models/CGame.ts
@@ -0,0 +1,12 @@
+import { Player } from './CPlayer';
+import { CSquare } from './CSquare';
+
+export class Game {
+  grid: CSquare[] = Array.from({ length: 9 }, () => new CSquare());
+  isDone: boolean = false;
+  isPlaying: Player;
+
+  constructor(public players: Player[]) {
+    this.isPlaying = this.players[Math.round(Math.random())];
+  }
+}
