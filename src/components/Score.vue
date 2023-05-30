ae62fbad36226ee4420b746959fc4f49ec264ddd
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-05-30 14:43:49 +0200

    added score render component

---
+++ src/components/Score.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Match } from '../models/CMatch';
+
+interface IProps {
+  score: Match;
+}
+
+const props = defineProps<IProps>();
+</script>
+
+<template>
+  <td>
+    {{ props.score.players[0].name }} vs {{ props.score.players[1].name }}
+  </td>
+  <td>
+    {{ props.score.players[0].score }} - {{ props.score.players[1].score }}
+  </td>
+</template>
