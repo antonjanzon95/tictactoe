c2b88a2b5bf580e07771a5f29c738938f4b1b69a
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-05-30 14:31:29 +0200

    added scores route

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from './views/HomeView.vue';
-import AboutView from './views/AboutView.vue';
 import GameView from './views/GameView.vue';
+import ScoreViewVue from './views/ScoreView.vue';
 
 const routes = [
   { path: '/', component: HomeView },
-  { path: '/about', component: AboutView },
+  { path: '/scores', component: ScoreViewVue },
   { path: '/game', component: GameView },
 ];
 
