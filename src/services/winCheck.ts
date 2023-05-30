84fed888a620840c6cf84fdd38e0f927af105019
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-05-30 14:43:06 +0200

    added win check func

--- src/services/winCheck.ts
+++ src/services/winCheck.ts
@@ -1,21 +1,28 @@
-// import { Game } from '../models/CGame';
-// import { CSquare } from '../models/CSquare';
+import { Game } from '../models/CGame';
+import { Shape } from '../models/Shape';
 
-// export const winningCombinations = [
-//   [0, 1, 2],
-//   [3, 4, 5],
-//   [6, 7, 8],
-//   [0, 3, 6],
-//   [1, 4, 7],
-//   [2, 5, 8],
-//   [0, 4, 8],
-//   [2, 4, 6],
-// ];
+export const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-// export const winCheck = (game: Game) => {
-//   winningCombinations.forEach((combination) => {
-//     combination.forEach((square, index) => {
-//       if
-//     })
-//   })
-// }
+export const winCheck = (game: Game) => {
+  let winningShape: Shape | null = null;
+  winningCombinations.forEach((combination) => {
+    if (
+      game.grid[combination[0]].shape &&
+      game.grid[combination[0]].shape == game.grid[combination[1]].shape &&
+      game.grid[combination[1]].shape == game.grid[combination[2]].shape
+    ) {
+      winningShape = game.grid[combination[0]].shape;
+    }
+  });
+
+  return winningShape;
+};
