ac8de59ed3f05bc88e11cfb4c656ae3909fd9b3a
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-05-30 14:43:59 +0200

    added scores table render

---
+++ src/components/Scores.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Match } from '../models/CMatch';
+import Score from './Score.vue';
+
+interface IProps {
+  scores: Match[];
+}
+const props = defineProps<IProps>();
+
+console.log(props.scores);
+</script>
+
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Match</th>
+        <th>Result</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="score in scores">
+        <Score :score="score" />
+      </tr>
+    </tbody>
+  </table>
+</template>
