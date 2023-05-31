a0701ea3353f8b3225fbf921693f2167f2e74535
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-05-31 16:43:50 +0200

    added game information & basic styling

--- src/components/GameInfo.vue
+++ src/components/GameInfo.vue
@@ -6,15 +6,20 @@ interface IProps {
 }
 
 const props = defineProps<IProps>();
-
-const emit = defineEmits(['resetGame', 'saveScore']);
 </script>
 
 <template>
-  <h3>
-    Current player: {{ props.game.isPlaying.name }} with
-    {{ props.game.isPlaying.shape }}
-  </h3>
-  <button @click="emit('resetGame')">Reset</button>
-  <button @click="emit('saveScore', props.game.players)">Save scores</button>
+  <div class="flex flex-col gap-6 justify-center items-center text-center">
+    <h2 class="p-4 bg-slate-800 rounded-xl text-teal-400 font-semibold text-xl">
+      Score <br />
+      {{ props.game.players[0].name }}: {{ props.game.players[0].score }} -
+      {{ props.game.players[1].name }}: {{ props.game.players[1].score }}
+    </h2>
+    <h3
+      class="p-4 bg-slate-800 rounded-xl text-orange-400 font-semibold text-lg"
+    >
+      Current player: {{ props.game.isPlaying.name }} with
+      {{ props.game.isPlaying.shape }}
+    </h3>
+  </div>
 </template>
