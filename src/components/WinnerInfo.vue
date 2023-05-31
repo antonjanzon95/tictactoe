43fde3d0bc28037f944e2ae5143638e99b67d2f7
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-05-31 16:44:33 +0200

    added winner info & btns for play again, save game

---
+++ src/components/WinnerInfo.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Game } from '../models/CGame';
+
+interface IProps {
+  winner: string;
+  game: Game;
+}
+
+const props = defineProps<IProps>();
+const emit = defineEmits(['playAgain', 'saveAndQuit']);
+</script>
+
+<template>
+  <div>
+    <h2>{{ props.winner }} has won!</h2>
+    <button @click="emit('playAgain')">Play again</button>
+    <button @click="emit('saveAndQuit', props.game.players)">
+      Save scores and quit
+    </button>
+  </div>
+</template>
+
+<style scoped></style>
