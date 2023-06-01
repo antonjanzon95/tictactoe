68921f6cdf85995d52a4d2bd876395ede1245cd4
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-06-01 12:48:23 +0200

    added content & styling home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Home</h1>
+  <div class="flex flex-col justify-center items-center gap-8">
+    <h1 class="text-5xl font-bold">Welcome to TicTacToe</h1>
+    <p class="text-xl">
+      To play, simply navigate to the
+      <router-link
+        to="/game"
+        class="text-orange-500 font-bold hover:text-orange-800 transition-all"
+        >game</router-link
+      >
+      section. Have fun!
+    </p>
+  </div>
 </template>
 
 <style scoped></style>
